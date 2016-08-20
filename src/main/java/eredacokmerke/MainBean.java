445f27dbf5ca657a4d7959ba8c6ae16f6691f1e6
eredacokmerke <eredacokmerke@gmail.com>
2016-08-20 15:49:41 +0300

    ana sinif eklendi

---
+++ src/main/java/eredacokmerke/MainBean.java
@@ -0,0 +1,13 @@
+package eredacokmerke;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class MainBean
+{
+    public MainBean()
+    {
+    }
+}
